8a7d2b901a2930a6cd16d9215fad9fce4dfa3e23
QQshkew <carlferder407@gmail.com>
2025-05-18 12:43:12 +0300

    Update ping-pong.py
    Σδελαλ τακ ψηοτι μυαψηικ οτψκακιωαλ ι ποφικςιλ παλκι

--- ping-pong/ping-pong.py
+++ ping-pong/ping-pong.py
@@ -4,7 +4,7 @@ from time import time as timer
 
 font.init()
 font1 = font.SysFont('Arial', 80)
-win = font1.render('YOU WIN!', True, (255, 255, 255))
+win = font1.render('YOU WIN!', True, (0, 0, 0))
 lose = font1.render('YOU LOSE!', True, (180, 0, 0))
 font2 = font.SysFont('Arial', 36)
 
@@ -37,13 +37,13 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.x < win_width - 200:
+        if keys[K_s] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.x < win_width - 80:
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
 
  
@@ -56,9 +56,9 @@ win_height = 500
 display.set_caption("Ping Pong")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
-racketl = Player(img_racket, 5, win_height - 100, 80, 100, 10)
-racketr = Player(img_racket, 600, win_height - 100, 80, 100, 10)
-ball = Ball(img_ball, 300, win_height - 200, 80, 80, 2)
+racketl = Player(img_racket, 5, win_height - 100, 50, 100, 5)
+racketr = Player(img_racket, 650, win_height - 100, 50, 100, 5)
+ball = Ball(img_ball, 300, win_height - 200, 50, 50, 2)
 
 while run:
     run = True
@@ -72,8 +72,11 @@ while run:
         ball.rect.x += speed_x
         ball.rect.y += speed_y
 
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+
         if sprite.collide_rect(racketl, ball) or sprite.collide_rect(racketr, ball):
-                speed_x *= -1
+            speed_x *= -1
 
         racketl.update_l()
         racketr.update_r()
